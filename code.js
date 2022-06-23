9a66a072899153bb04a5f8206a2b6c714094571f
YoYoDough <yosuf818s@gmail.com>
2022-06-22 23:40:27 -0700

    Now I am trying to figure out how to make the output equal to the sum

--- code.js
+++ code.js
@@ -1,25 +1,82 @@
 var container = document.getElementById('container');
 var output = document.querySelector('.output')
 var divisibles = ['+', '-', '/', '*', '=']
-for (i = 0; i < 5; i++)
+var clear = document.getElementById('clear')
+var a = 0;
+var b = 0;
+// for (i = 0; i < 5; i++)
+// {
+//     var operators = document.createElement('button');
+//     operators.classList.add('operators');
+//     operators.textContent = divisibles[i];
+//     container.appendChild(operators);
+//     operators.addEventListener('click' , function(event)
+//    {
+//         output.textContent = event.target.textContent;
+//    });
+// }
+// for(i = 0; i < 10; i++)
+// {
+//    var number = document.createElement('button');
+//    number.classList.add('box');
+//    number.textContent = i;
+//    number.addEventListener('click' , function(event)
+//    {
+//         output.textContent = event.target.textContent;
+//    });
+//    container.appendChild(number);
+// }
+function makeButtonsAndOutput(number, operators)
 {
-    var operators = document.createElement('button');
-    operators.classList.add('operators');
-    operators.textContent = divisibles[i];
-    container.appendChild(operators);
-    operators.addEventListener('click' , function(event)
-   {
-        output.textContent = event.target.textContent;
-   });
+     for (i = 0; i < 5; i++)
+     {
+          var operators = document.createElement('button');
+          operators.classList.add('operators');
+          operators.textContent = divisibles[i];
+          container.appendChild(operators);
+          operators.addEventListener('click' , function(event)
+          {
+               output.textContent = event.target.textContent;
+          });
+     }
+     for(i = 0; i < 10; i++)
+     {
+          var number = document.createElement('button');
+          number.classList.add('box');
+          number.textContent = i;
+          number.addEventListener('click' , function(event)
+          {
+               output.textContent += event.target.textContent;
+               a = parseInt(output.textContent);
+          });
+          container.appendChild(number);
+     }
 }
-for(i = 0; i < 10; i++)
-{
-   var number = document.createElement('button');
-   number.classList.add('box');
-   number.textContent = i;
-   number.addEventListener('click' , function(event)
-   {
-        output.textContent = event.target.textContent;
-   });
-   container.appendChild(number);
-}
+     clear.addEventListener('click', function()
+     {
+          output.textContent = ''
+     });
+     
+     function add(a)
+     {
+          sum = 0;
+          return sum += a;
+     }
+     
+     function subtract(a)
+     {
+          return a-b;
+     }
+
+     function multiply(a, b)
+     {
+          return a*b
+     }
+
+     function divide(a, b)
+     {
+          return a/b;
+     }
+
+
+makeButtonsAndOutput();
